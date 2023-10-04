32d267569d3e4db698321ec1441d29b2e434c1eb
driftmaniacc <114745277+driftmaniacc@users.noreply.github.com>
2023-10-04 22:45:55 +0530

    Create bubble_sort.cpp

---
+++ c++/bubble_sort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main() {
+    int size;
+
+    cout << "Enter the size of the array : " << endl;
+    cin >> size;
+
+    int arr[size];
+    cout << "Enter the elements of the array : " << endl;
+    for (int i = 0; i < size; ++i) {
+        cin >> arr[i];
+    }
+
+    bubbleSort(arr, size);
+
+    cout << "Sorted array : " << endl;
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
